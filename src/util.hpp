1a77275249533e9cd82bbb14c51ca4919b789347
Amar Tabakovic <tabakovicamar1@gmail.com>
2024-12-02 22:46:14 +0100

    Add timer

--- src/util.hpp
+++ src/util.hpp
@@ -3,6 +3,7 @@
 
 // Commonly used headers
 #include <algorithm>
+#include <chrono>
 #include <cmath>
 #include <fstream>
 #include <functional>
@@ -18,6 +19,56 @@
 #include <vector>
 
 namespace util {
+
+class Timer
+{
+  enum class TimerState { kReset, kStarted, kStopped };
+
+public:
+  Timer() = default;
+  ~Timer() = default;
+
+  void Start()
+  {
+    if (state_ == TimerState::kStarted) {
+      throw std::runtime_error("Timer is already running.");
+    }
+
+    t1_ = std::chrono::high_resolution_clock::now();
+    state_ = TimerState::kStarted;
+  }
+
+  void Stop()
+  {
+    if (state_ != TimerState::kStarted) {
+      throw std::runtime_error("Timer must be started first to be stopped.");
+    }
+
+    t2_ = std::chrono::high_resolution_clock::now();
+    state_ = TimerState::kStopped;
+  }
+
+  void Reset()
+  {
+    t1_ = {};
+    t2_ = {};
+    state_ = TimerState::kReset;
+  }
+
+  double GetTimeMs()
+  {
+    if (state_ != TimerState::kStopped) {
+      throw std::runtime_error("Timer must be stopped first to get the elapsed time.");
+    }
+    return std::chrono::duration<double, std::milli>(t2_ - t1_).count();
+  }
+
+private:
+  using TimePoint = std::chrono::time_point<std::chrono::high_resolution_clock>;
+
+  TimePoint t1_, t2_;
+  TimerState state_{TimerState::kReset};
+};
 
 std::vector<std::string> ReadInput(const std::string &path)
 {
