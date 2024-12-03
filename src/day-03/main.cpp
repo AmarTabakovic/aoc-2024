0e1d02387bfd9e7ec19cdef9b5ecf73459d8d747
Amar Tabakovic <tabakovicamar1@gmail.com>
2024-12-03 08:22:19 +0100

    Day 3

--- src/day-03/main.cpp
+++ src/day-03/main.cpp
@@ -1,11 +1,48 @@
 #include "util.hpp"
 
-int Part1(const std::vector<std::string> &input) {
-  return 0;
+int Part1(const std::vector<std::string> &input)
+{
+  int sum = 0;
+
+  std::regex regex{R"(mul\([0-9]{1,4},[0-9]{1,4}\))"};
+
+  for (const auto &line : input) {
+    for (auto it = std::sregex_iterator(line.begin(), line.end(), regex);
+         it != std::sregex_iterator{};
+         it++) {
+      int a{0}, b{0};
+      std::sscanf((*it).str().data(), "mul(%d,%d)", &a, &b);
+      sum += a * b;
+    }
+  }
+  return sum;
 }
 
 int Part2(const std::vector<std::string> &input) {
-  return 0;
+  int sum = 0;
+
+  std::regex regex{R"((mul\([0-9]{1,4},[0-9]{1,4}\))|(do\(\))|(don't\(\)))"};
+  bool mul_enabled{true};
+
+  for (const auto &line : input) {
+    for (auto it = std::sregex_iterator(line.begin(), line.end(), regex);
+         it != std::sregex_iterator{};
+         it++) {
+      auto s = (*it).str();
+      if (s == "don't()") {
+        mul_enabled = false;
+      } else if (s == "do()") {
+        mul_enabled = true;
+      } else {
+        if (mul_enabled) {
+          int a{0}, b{0};
+          std::sscanf(s.data(), "mul(%d,%d)", &a, &b);
+          sum += a * b;
+        }
+      }
+    }
+  }
+  return sum;
 }
 
 int main() {
